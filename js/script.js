4bb297da7e4f84d2ab00ef7baa7779fc56460011
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-08-09 17:51:08 +0200

    Add second button

--- js/script.js
+++ js/script.js
@@ -8,12 +8,36 @@ var temp;
 var celsius
 var fahrenheit;
 
-btn.addEventListener('click', function() {
+btnC.addEventListener('click', function() {
   celsius = parseInt(window.prompt('Enter the temperature in Celsius'));
   if (!isNaN(celsius)) {
     fahrenheit = (celsius * 1.8) + 32;
     output.innerHTML = celsius +'&#176C is equal ' + 
-    Math.floor(fahrenheit) + '&#176F Fahrenheit' +'<br><br>';
+    Math.floor(fahrenheit) + '&#176F <br><br>';
+    
+    if (celsius <= -28) {
+      output.innerHTML += 'It\'s freezing. Stay at home!';
+    } else  if (celsius <= 12) {
+      output.innerHTML += 'It\'s cold. Take something with long sleeves.';
+    } else if (celsius >= 28) {
+      output.innerHTML += 'Don\'t forget about something to drink.<br>'+
+      ' It\'s hot outside.';
+    } else {
+      output.innerHTML += 'The temperature is average.';
+    }
+
+  } else {
+    output.innerHTML = 'Wrong value';
+  }
+})
+
+btnF.addEventListener('click', function() {
+  fahrenheit = parseInt(window.prompt('Enter the temperature in Fahrenheit'));
+  if (!isNaN(fahrenheit)) {
+    celsius = (fahrenheit - 32) * (5/9);
+    console.log(celsius);
+    output.innerHTML = fahrenheit +'&#176F is equal ' + 
+    Math.floor(celsius) + '&#176C <br><br>';
     
     if (celsius <= -28) {
       output.innerHTML += 'It\'s freezing. Stay at home!';
