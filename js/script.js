0f905d2078db9f817bd9799b5b660e2e8f47eeea
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-08-09 17:34:17 +0200

    Add extra mgs

--- js/script.js
+++ js/script.js
@@ -5,16 +5,28 @@ var output = document.getElementById('output');
 var btn = document.getElementById('btn');
 
 var temp;
-var converted;
+var celsius
+var fahrenheit;
 
 btn.addEventListener('click', function() {
-  temp = parseInt(window.prompt('Enter the temperature in Celsius'));
-  if (!isNaN(temp)) {
-    converted = (temp * 1.8) + 32;
-    console.log(converted);
-    output.innerHTML = temp +"&#176C is equal " + 
-    Math.floor(converted) + "&#176F Fahrenheit";
+  celsius = parseInt(window.prompt('Enter the temperature in Celsius'));
+  if (!isNaN(celsius)) {
+    fahrenheit = (celsius * 1.8) + 32;
+    output.innerHTML = celsius +'&#176C is equal ' + 
+    Math.floor(fahrenheit) + '&#176F Fahrenheit' +'<br><br>';
+    
+    if (celsius <= -28) {
+      output.innerHTML += 'It\'s freezing. Stay at home!';
+    } else  if (celsius <= 12) {
+      output.innerHTML += 'It\'s cold. Take something with long sleeves.';
+    } else if (celsius >= 28) {
+      output.innerHTML += 'Don\'t forget about something to drink.<br>'+
+      ' It\'s hot outside.';
+    } else {
+      output.innerHTML += 'The temperature is average.';
+    }
+
   } else {
-    output.innerHTML = "Wrong value";
+    output.innerHTML = 'Wrong value';
   }
 })
