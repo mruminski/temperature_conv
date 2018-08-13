67f53609725515dac603ce8668eab23a5303ceac
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-08-13 17:22:53 +0200

    Add Mateusz's sugestions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 'use strict';
 
-// define html elements
 var output = document.getElementById('output');
 var btn = document.getElementById('btn');
 
-// variables
 var celsius;
 var fahrenheit;
 
-// get celsius and return fahrenheit
 var getFahrenheit = function(celsius) {
   fahrenheit = Math.floor((celsius * 1.8) + 32);
   return fahrenheit;
 }
 
-// get celsius and return message
 var getMsg = function(celsius) {
   if (celsius <= -28) {
     return 'It\'s freezing. Stay at home!';
@@ -23,25 +19,21 @@ var getMsg = function(celsius) {
   } else if (celsius >= 28) {
     return 'Don\'t forget about something to drink.<br>'+
     ' It\'s hot outside.';
-  } else {
-    return 'The temperature is average.';
   }
+    return 'The temperature is average.';
 }
 
-// get fahrenheit and return celsius
 var getCelsius = function(fahrenheit) {
   celsius = Math.floor((fahrenheit - 32) * (5/9));
   return celsius;
 }
 
-// display text on the page and add a new line
 var printOnPage = function() {
   output.innerHTML = '<br>' + celsius + ' &#176C is equal '
   + fahrenheit + ' &#176F <br>' + getMsg(celsius)
   + '<br>';
 }
 
-// click buttons action
 var convCelsius = function() {
   celsius = parseInt(window.prompt('Enter the temperature in Celsius'));
   if (!isNaN(celsius)) {
